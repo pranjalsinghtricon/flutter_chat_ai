90fb19cd8a3e5fd1661573ce4678dbafe4114c11
Pranjal Singh <pranjal.singh@triconinfotech.com>
2025-08-11 15:09:35 +0530

    Task : custom icon button, custom dropdown, custom-user-query-card

--- lib/features/chat/presentation/widgets/message_bubble.dart
+++ lib/features/chat/presentation/widgets/message_bubble.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ai/common_ui_components/cards/custom_ai_response_card.dart';
 import 'package:flutter_chat_ai/data/models/message_model.dart';
+import 'package:flutter_chat_ai/common_ui_components/cards/custom_user_query_card.dart';
 
 class MessageBubble extends StatelessWidget {
   final Message message;
@@ -10,45 +11,9 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (message.isUser) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Initials box
-            Container(
-              width: 36,
-              height: 36,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.blue, width: 1),
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: Text(
-                 "AR", // Add senderInitials in your model
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            const SizedBox(width: 8),
-            // Message bubble
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(6),
-                border: Border.all(color: Colors.grey.shade300),
-              ),
-              child: Text(
-                message.content,
-                style: const TextStyle(fontSize: 14, color: Colors.black87),
-              ),
-            ),
-          ],
-        ),
+      return CustomUserQueryCard(
+        initials: "AR",
+        message: message.content,
       );
     } else {
       return CustomAiResponseCard(message: message.content);
