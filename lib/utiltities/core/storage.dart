369f09b86cdbc72b9587f904b24ad6cc2d4560f1
pranjal.singh <pranjal.singh@triconinfotech.com>
2025-09-19 14:13:57 +0530

    task : storage in utilities

---
+++ lib/utiltities/core/storage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenStorage {
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> saveAccessToken(String token) async {
+    await _storage.write(key: 'access_token', value: token);
+  }
+
+  Future<String?> getAccessToken() async {
+    return await _storage.read(key: 'access_token');
+  }
+
+  Future<void> clearAccessToken() async {
+    await _storage.delete(key: 'access_token');
+  }
+}
